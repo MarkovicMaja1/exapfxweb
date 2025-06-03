34f16f8595498d140644fbd6d8cba7ece79ec1e9
nsladoje <nsladoje@gmail.com>
2025-06-03 23:51:19 +0200

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,12 +26,17 @@ const AboutUs = () => {
                     <p className="text-lg text-white mb-4">
                       But we’re more than just a prop firm. We believe that financial growth and sustainability can go hand in hand. That’s why we commit 1-3% of our annual profits to sustainability (SDGS)-focused initiatives, ensuring that our success contributes to a better future for both traders and the planet.
                     </p>
+                    <h3 className="text-3xl font-semibold mb-4 inline-block">Our Mission</h3>
+                    <p className="text-lg text-white">
+                      We are on a mission to build the largest and most impactful proprietary trading firm, one that not only fuels the success of traders but also fosters a strong, values-driven community over time. We aim to support ambitious traders in reaching their full potential while integrating responsible practices that promote a sustainable future.
+                    </p>
                   </div>
                 </dl>
               </div>
 
               <div className="about-us-image ecoImage flex justify-center mt-10">
-                <img src={image1} alt="About Us" loading="lazy" className="w-3/4 h-3/4 rounded-lg bg-transparent hover:scale-105 transition-transform duration-300" />
+                <img src={image2} alt="About Us" loading="lazy" className="w-3/4 h-3/4 mr-10 rounded-lg bg-transparent hover:scale-105 transition-transform duration-300" />
+                {/* <img src={image1} alt="About Us" loading="lazy" className="w-3/4 h-3/4 rounded-lg bg-transparent hover:scale-105 transition-transform duration-300" /> */}
               </div>
             </div>
           </div>
@@ -42,16 +47,16 @@ const AboutUs = () => {
           <div className="max-w-7xl mx-auto px-6 lg:px-12">
             <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-16 about-us-grid">
                   <div className="text-left">
-                    <h3 className="text-3xl font-semibold mb-4 inline-block">Our Mission</h3>
+                    {/* <h3 className="text-3xl font-semibold mb-4 inline-block">Our Mission</h3>
                     <p className="text-lg text-white">
                       We are on a mission to build the largest and most impactful proprietary trading firm, one that not only fuels the success of traders but also fosters a strong, values-driven community over time. We aim to support ambitious traders in reaching their full potential while integrating responsible practices that promote a sustainable future.
-                    </p>
+                    </p> */}
                   </div>
 
               <div className="about-us-text mx-auto sm:mt-20 lg:max-w-4xl text-left">
                 <dl className="grid grid-cols-1 gap-y-16">
                   <div className="about-us-image missionImage flex justify-center mr-10">
-                    <img src={image2} alt="About Us" loading="lazy" className="w-3/4 h-3/4 mr-10 rounded-lg bg-transparent hover:scale-105 transition-transform duration-300" />
+                    {/* <img src={image2} alt="About Us" loading="lazy" className="w-3/4 h-3/4 mr-10 rounded-lg bg-transparent hover:scale-105 transition-transform duration-300" /> */}
                   </div>
                 </dl>
               </div>
