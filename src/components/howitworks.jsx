44d9ec18268750e73fbbda2f28301de00385ff4f
nsladoje <nsladoje@gmail.com>
2025-08-10 19:52:55 +0200

    fix

--- src/components/howitworks.jsx
+++ src/components/howitworks.jsx
@@ -65,7 +65,7 @@ const HowItWorks = () => {
       /*  Hero                                                              */
       /* ------------------------------------------------------------------ */}
       <motion.div
-        className="mx-auto max-w-7xl mt-5 px-4 py-24 sm:px-6 lg:px-8 lg:py-24 relative z-10 text-center"
+        className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 lg:py-6 relative z-10 text-center"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, ease: [0.25, 0.1, 0.25, 1] }}
